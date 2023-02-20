9bc831214410aaa6673e5a993ad0f514d1991280
KalYvesGit <kalivayves>
2023-02-20 18:18:45 +0300

    Initial Commit

---
+++ Reminder App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder App
+//
+//  Created by Jean Yves Kaliva on 2/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
